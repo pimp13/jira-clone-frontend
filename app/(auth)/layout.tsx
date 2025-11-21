0b02771acbe52c55a7c4bacd93d9093aee7157e8
pimp13 <pimp.puma.13@gmail.com>
2025-11-21 15:51:23 +0330

    Add authentication page design, implement backend communication with SWR and Axios, and enhance sign-in card layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 type AuthLayoutProps = {
   children: Readonly<React.ReactNode>;
