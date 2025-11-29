51718e59b4a8287311fc3afaa6e0689b15e33991
pimp13 <pimp.puma.13@gmail.com>
2025-11-29 15:11:29 +0330

    Implement SignUpCard component and update SignUpPage to use it

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,7 @@
+import { SignUpCard } from '@/features/auth/components/sign-up-card';
+
 const SignUpPage = () => {
-  return ( 
-    <div>SignUp Page</div>
-   );
-}
- 
+  return <SignUpCard />;
+};
+
 export default SignUpPage;
