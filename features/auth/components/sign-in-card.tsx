5b6bdd7ff871b9b14c40b7109769b4ea4b424b5a
pimp13 <pimp.puma.13@gmail.com>
2025-11-29 14:34:46 +0330

    Add react-icons dependency, enhance SignInCard component with email and password inputs, and implement login buttons

--- features/auth/components/sign-in-card.tsx
+++ features/auth/components/sign-in-card.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import { DottedSeparator } from '@/components/dotted-separator';
+import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
 
 export const SignInCard = () => {
   return (
@@ -8,12 +12,48 @@ export const SignInCard = () => {
         <CardTitle className="text-2xl">Welcome Back...</CardTitle>
       </CardHeader>
 
-      <div className="px-7 mb-2">
+      <div className="px-7">
         <DottedSeparator />
       </div>
 
-      <CardContent>
-        <div>Hello world this is a sign in page...</div>
+      <CardContent className="p-7">
+        <form className="space-y-4">
+          <Input
+            required
+            type="email"
+            value={''}
+            onChange={() => {}}
+            placeholder="Enter email address"
+            disabled={false}
+          />
+          <Input
+            required
+            type="password"
+            value={''}
+            onChange={() => {}}
+            placeholder="Enter password"
+            disabled={false}
+            min={8}
+            max={256}
+          />
+
+          <Button disabled={false} size="lg" className="w-full">
+            Login
+          </Button>
+        </form>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full"
+        >
+          Login with Google
+        </Button>
       </CardContent>
     </Card>
   );
