385ab58ccce92d9fb54a9d1c986497c289d0124c
pimp13 <pimp.puma.13@gmail.com>
2025-12-13 17:19:07 +0330

    Implement CreateWorkspaceForm component with Zod validation schema and React Hook Form integration for workspace creation.

--- features/workspaces/components/create-workspace-form.tsx
+++ features/workspaces/components/create-workspace-form.tsx
@@ -1,11 +1,23 @@
+import z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { createWorkspaceSchema } from '../schemas';
+
 interface CreateWorkspaceFormProps {
   onCancel: () => void;
 }
 
+export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
+  const form = useForm<z.infer<typeof createWorkspaceSchema>>({
+    resolver: zodResolver(createWorkspaceSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
 
-export const CreateWorkspaceForm = () => {
-  return (
-    <div>CreateWorkspaceForm</div>
-  )
-}
+  const onSubmit = (values: z.infer<typeof createWorkspaceSchema>) => {
+    console.info('values =>', values);
+  };
 
+  return <div>CreateWorkspaceForm</div>;
+};
