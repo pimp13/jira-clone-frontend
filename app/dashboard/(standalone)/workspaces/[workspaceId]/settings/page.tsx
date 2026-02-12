86ecfad487fa076aa6ec5c685b369bf749eb5b65
pimp13 <pimp.puma.13@gmail.com>
2026-02-12 17:10:29 +0330

    Comment out EditWorkspaceForm in WorkspaceSettingPage for future implementation.

--- app/dashboard/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ app/dashboard/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -10,7 +10,7 @@ const WorkspaceSettingPage = async ({ params }: WorkspaceSettingProps) => {
   const { workspaceId } = await params;
   return (
     <div>
-      <EditWorkspaceForm />
+      {/* <EditWorkspaceForm /> */}
     </div>
   );
 };
