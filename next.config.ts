4688e6045a788647ee93683c11bcab6e875359f5
pimp13 <pimp.puma.13@gmail.com>
2026-02-16 19:51:59 +0330

    change base path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig = {
+  basePath: '/jira',
+  assetPrefix: '/jira/',
   images: {
     remotePatterns: [
       {
@@ -14,6 +16,12 @@ const nextConfig = {
         hostname: 'http://127.0.0.1',
         port: '1152',
         pathname: '/**',
+      },
+      {
+        protocol: "https",
+        hostname: "https://127.0.0.1",
+        port: "4224",
+        pathname: "/**",
       },
     ],
   },
